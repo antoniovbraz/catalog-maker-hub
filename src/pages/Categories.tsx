81c3bb7fe5ec1f1e56a3a327413aee147bf48059
Antonio Braz <antoniovbraz@gmail.com>
2025-08-03 11:18:20 -0300

    refactor: use configuration page layout

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,21 +1,24 @@
 import { CategoryForm } from "@/components/forms/CategoryForm";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ConfigurationPageLayout } from "@/components/layout/ConfigurationPageLayout";
+import { FolderTree } from "@/components/ui/icons";
 
 const Categories = () => {
+  const breadcrumbs = [
+    { label: "Configurações", href: "/dashboard" },
+    { label: "Categorias" }
+  ];
+
   return (
-    <div className="p-lg">
-      <Card>
-        <CardHeader>
-          <CardTitle>Gerenciar Categorias</CardTitle>
-          <CardDescription>
-            Organize seus produtos em categorias para melhor gestão
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <CategoryForm />
-        </CardContent>
-      </Card>
-    </div>
+    <ConfigurationPageLayout
+      title="Gerenciar Categorias"
+      description="Organize seus produtos em categorias para melhor gestão"
+      icon={<FolderTree className="w-6 h-6" />}
+      breadcrumbs={breadcrumbs}
+    >
+      <div className="xl:col-span-6">
+        <CategoryForm />
+      </div>
+    </ConfigurationPageLayout>
   );
 };
 
